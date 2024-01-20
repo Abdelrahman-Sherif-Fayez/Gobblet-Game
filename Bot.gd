269774cba32d7bafa7a0f68690d6f6a65dbcc549
Ahmed Reda <ahmedreda4000@yahoo.com>
2024-01-20 13:07:55 +0200

    Testing functions
    branch to test functions if they work or not

--- Bot.gd
+++ Bot.gd
@@ -5,45 +5,50 @@ var bitboard
 func set_board(bitboardGame):
 	bitboard = bitboardGame
 
-func play_next_move():
+func play_next_move(possible_moves, move, color):
+
+    var new_moves = possible_moves.copy()
+    new_moves.append((move, color))
+
+    return new_moves
 	#return Move.new(-1, 3, 100*100, 0) # for testing update_board (Working)
 	pass
-#will use search moves function and use make move function
-#will update the bitboard with the move 
+	#will use search moves function and use make move function
+	#will update the bitboard with the move 
 
-#func search_moves(isBlackMove, possible_moves, depth, alpha, beta):
+func search_moves(isBlackMove, possible_moves, depth, alpha, beta):
 	##will use generate move set fuction recursively and will prune and use eval function 
 	##to select best move acc to prunning
 	##still needs to be turned to gadot 
-	#if depth == 0 or not possible_moves:
-		#return evaluate_position(isBlackMove, possible_moves), None
+	if depth == 0 or not possible_moves:
+		return evaluate_position(isBlackMove, possible_moves), None
 #
-	#if not isBlackMove:
-		#max_eval = float('-inf')
-		#best_move = None
-		#for move in possible_moves:
-			#new_possible_moves = generate_moves(apply_move(possible_moves, move), color)
-			#eval, _ = search_moves(new_possible_moves, depth - 1, alpha, beta, False, color)
-			#max_eval = max(max_eval, eval)
-			#alpha = max(alpha, eval)
-			#if beta <= alpha:
-				#break  # Beta cut-off
-			#if eval == max_eval:
-				#best_move = move
-		#return max_eval, best_move
-	#else:
-		#min_eval = float('inf')
-		#best_move = None
-		#for move in possible_moves:
-			#new_possible_moves = generate_moves(apply_move(possible_moves, move), color)
-			#eval, _ = minimax_with_alpha_beta_pruning_moves(new_possible_moves, depth - 1, alpha, beta, True, color)
-			#min_eval = min(min_eval, eval)
-			#beta = min(beta, eval)
-			#if beta <= alpha:
-				#break  # Alpha cut-off
-			#if eval == min_eval:
-				#best_move = move
-		#return min_eval, best_move
+	if not isBlackMove:
+		var max_eval = float('-inf')
+		var min_eval = float('inf')
+		var best_move = None
+		for move in possible_moves: 
+			var new_possible_moves = generate_move(play_next_move(possible_moves, move, color), color)
+			var eval, _ = search_moves(new_possible_moves, depth - 1, alpha, beta, False, color)
+			max_eval = max(max_eval, eval)
+			var alpha = max(alpha, eval)
+			if beta <= alpha:
+				break  # Beta cut-off
+			if eval == max_eval:
+				best_move = move
+		return max_eval, best_move
+	else:
+		best_move = None
+		for move in possible_moves:
+			var new_possible_moves = generate_moves(apply_move(possible_moves, move), color)
+			var eval, _ = minimax_with_alpha_beta_pruning_moves(new_possible_moves, depth - 1, alpha, beta, True, color)
+			min_eval = min(min_eval, eval)
+			var beta = min(beta, eval)
+			if beta <= alpha:
+				break  # Alpha cut-off
+			if eval == min_eval:
+				best_move = move
+		return min_eval, best_move
 #
 #func evaluate_position(isBlackMove, bitboard)-> float:
 	#var final_score = 0
